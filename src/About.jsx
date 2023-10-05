f7c30e7aa1ae16db37bf329c47a4103696361948
HanorahsStudio <Hanorah_murphy@hotmail.com>
2023-10-05 10:53:08 +0100

    Finish contact form

--- src/About.jsx
+++ src/About.jsx
@@ -4,50 +4,59 @@ import { Link } from "react-router-dom";
 export default function About() {
   return (
     <div className="max-w-[800px] flex flex-col items-center gap-8  md:gap-16 px-4 mx-auto my-[40px]">
-      <h1 className="text-center text-xl sm:text-2xl md:text-3xl md:mt-4 font-semibold">ABOUT ME</h1>
+      <h1 className="text-center text-xl sm:text-2xl md:text-3xl md:mt-4 font-semibold">
+        ABOUT ME
+      </h1>
 
-      <h2 className="w-full font-semibold text-xl md:text-4xl mt-16">HANORAH MURPHY</h2>
+      <h2 className="w-full font-semibold text-xl md:text-4xl mt-16">
+        HANORAH MURPHY
+      </h2>
 
       <div className="text-sm md:text-lg">
         <p>
-          Hi, I'm Hanorah, I’m an undergraduate Graphic Communication Student
-          and will be graduating this coming summer. Alongside my studies I run
-          my own freelance design studio. I am a goal-driven graphic design
-          artist, with a passion for design. I am successful at applying
-          technical skills to create pieces that informs and engages. During the
-          summer between second and third year, I completed an internship at
-          ClearBrand where I designed and prototyped a website following a
-          rebranding. Additionally I created an introduction animation for
-          Reading Borough council. Both of which can be found within my
-          portfolio. I am invested in all aspects of design but, especially
-          passionate about visual design, and interactive design however, I am
-          constantly exploring to widen my knowledge within design practices. I
-          have a can do attitude and feel every piece of work is an opportunity
-          to learn and further my knowledge.
+          Hi, I'm Hanorah, I am a First Class Graphic Communication Graduate
+          from the University of Reading.
         </p>
         <br />
         <p>
-          Alongside this I have carried out a range of different projects at
-          University. Which pushed my understanding of the Adobe Creative Suite,
-          with a great experience in Photoshop, Illustrator, Indesign and XD. I
-          have developed knowledge and understanding towards the theory of
-          graphic design as well. Understanding typographic and layout
-          principles and contemporary print production processes.​​​​​​​{" "}
+          I am a goal-driven graphic designer, with a passion for design. I am
+          successful at applying technical skills to create pieces that informs
+          and engages.
+        </p>
+        <br />
+        <p>
+          I have recently started my first role as a Graphic Design Graduate at
+          Sky Creative.
         </p>
       </div>
 
-      <Link className="bg-black hover:bg-black/70 duration-300 text-white font-bold rounded-full py-2 px-6" to="/">
+      <Link
+        className="bg-black hover:bg-black/70 duration-300 text-white font-bold rounded-full py-2 px-6"
+        to="/"
+      >
         View my work
       </Link>
 
       <div className="flex justify-center text-xl p-2 items-center gap-6">
-        <a href="http://linkedin.com/in/hanorah-murphy" target="_blank" rel="noreferrer">
+        <a
+          href="http://linkedin.com/in/hanorah-murphy"
+          target="_blank"
+          rel="noreferrer"
+        >
           <FaLinkedin />
         </a>
-        <a href="https://www.instagram.com/hanorahs.studio/" target="_blank" rel="noreferrer">
+        <a
+          href="https://www.instagram.com/hanorahs.studio/"
+          target="_blank"
+          rel="noreferrer"
+        >
           <FaInstagram />
         </a>
-        <a href="mailto:Hanorahs.studio@gmail.com" target="_blank" rel="noreferrer">
+        <a
+          href="mailto:Hanorahs.studio@gmail.com"
+          target="_blank"
+          rel="noreferrer"
+        >
           <FaEnvelope />
         </a>
       </div>
--- src/Contact.jsx
+++ src/Contact.jsx
@@ -1,27 +1,105 @@
+import React, { useRef, useState } from "react";
+import emailjs from "@emailjs/browser";
+
 export default function Contact() {
+  const form = useRef();
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+  const [confirm, setConfirm] = useState(false);
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+    console.log(name, email, message);
+    emailjs
+      .sendForm(
+        "service_wtw8kfe",
+        "template_bhpfy01",
+        form.current,
+        "nOTVrX1hoFyEN-d1K"
+      )
+      .then(
+        (result) => {
+          setConfirm(true);
+          setTimeout(function () {
+            setConfirm(false);
+          }, 2000);
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+    setName("");
+    setEmail("");
+    setMessage("");
+  };
+
   return (
     <div className="max-w-[600px] flex flex-col items-center gap-4 md:gap-8 px-4 mx-auto my-[40px]">
-      <h1 className="text-center text-xl sm:text-2xl md:text-3xl md:mt-4 font-semibold">Contact</h1>
-      <p className="text-center text-gray-600">Please fill out the quick form and I will be in touch as soon as I can!</p>
-      <form className="flex flex-col gap-4 my-8 w-full" action="">
+      <h1 className="text-center text-xl sm:text-2xl md:text-3xl md:mt-4 font-semibold">
+        Contact
+      </h1>
+      <p className="text-center text-gray-600">
+        Please fill out the quick form and I will be in touch as soon as I can!
+      </p>
+      <form
+        className="flex flex-col gap-4 my-8 w-full"
+        ref={form}
+        onSubmit={sendEmail}
+      >
         <div>
-            <label className="font-bold" htmlFor="">Name *</label>
-            <input type="text" className="border p-2 mt-1 w-full rounded-md" 
-            placeholder="Your Name..."/>
+          <label className="font-bold" htmlFor="">
+            Name *
+          </label>
+          <input
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            required
+            type="text"
+            name="user_name"
+            className="border p-2 mt-1 w-full rounded-md"
+            placeholder="Your Name..."
+          />
         </div>
         <div>
-            <label className="font-bold" htmlFor="">Email Address *</label>
-            <input type="text" className="border p-2 mt-1 w-full rounded-md" 
-            placeholder="Your Name..."/>
+          <label className="font-bold" htmlFor="">
+            Email Address *
+          </label>
+          <input
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+            type="email"
+            name="user_email"
+            className="border p-2 mt-1 w-full rounded-md"
+            placeholder="Your Name..."
+          />
         </div>
         <div>
-            <label className="font-bold" htmlFor="">Message *</label>
-            <textarea type="text" rows={6} className="border p-2 mt-1 w-full rounded-md" 
-            placeholder="Your Name..."/>
+          <label className="font-bold" htmlFor="">
+            Message *
+          </label>
+          <textarea
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+            required
+            name="message"
+            rows={6}
+            className="border p-2 mt-1 w-full rounded-md"
+            placeholder="Your Name..."
+          />
         </div>
-        <button className="bg-black hover:bg-black/70 duration-300 rounded-full w-28 py-2 text-white">
+        <div className="flex justify-between">
+          <button
+            type="submit"
+            value="Send"
+            className="bg-black hover:bg-black/70 duration-300 rounded-full w-28 py-2 text-white"
+          >
             Submit
-        </button>
+          </button>
+          {confirm && "Message sent ✔️"}
+        </div>
       </form>
     </div>
   );
